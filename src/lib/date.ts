69fdd4e0026afddd022e3f00c0b71da0c31cb157
Tim Hahn <hi@tmhhn.com>
2024-02-18 20:49:48 -0500

    Did some UI work

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -3,10 +3,13 @@ function join(date, options, separator) {
 		let formatter = new Intl.DateTimeFormat('en', option);
 		return formatter.format(date);
 	}
-	return options.map(format).map((s) => s.padStart(2, '0')).join(separator);
+	return options
+		.map(format)
+		.map((s) => s.padStart(2, '0'))
+		.join(separator);
 }
 export function formatDate(date: Date) {
-	let options = [{year: 'numeric'}, {month: 'numeric'}, {day: 'numeric'}];
-	let joined = join(new Date, options, '-');
-	return joined
+	let options = [{ year: 'numeric' }, { month: 'numeric' }, { day: 'numeric' }];
+	let joined = join(new Date(), options, '-');
+	return joined;
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,14 +16,15 @@
 	let currentLevel = 0;
 
 	$: suggestion = closest(word, getObjects(wordList, word));
-	$: dist = distance(word, suggestion);
 	$: showSuggestion = word.length > 0;
 	$: challenge = data && 'challenges' in data ? data['challenges'][levels[currentLevel]] : null;
 	$: answer = challenge ? challenge.words.map((word) => word.word) : [];
 	$: words = challenge ? challenge.words : [];
 
 	onMount(async () => {
-		const res = await fetch(`https://iamdreamingof.nyc3.cdn.digitaloceanspaces.com/today.json?date=${formatDate(new Date())}`);
+		const res = await fetch(
+			`https://iamdreamingof.nyc3.cdn.digitaloceanspaces.com/today.json?date=${formatDate(new Date())}`
+		);
 		data = await res.json();
 	});
 
@@ -76,36 +77,77 @@
 		correctWords = [];
 		guessedWords = [];
 	}
+
+	function getGuessedWords(guessedWords: string[]): string {
+		if (guessedWords.length === 1) {
+			return guessedWords[0];
+		} else if (guessedWords.length === 2) {
+			return `${guessedWords[0]} or ${guessedWords[1]}`;
+		} else {
+			return `${guessedWords.slice(0, -1).join(', ')}, or ${guessedWords.slice(-1)}`;
+		}
+	}
 </script>
-{#if challenge}
-	<picture>
-		<source type="image/webp" srcset={challenge['image_url_webp']} />
-		<source type="image/jpeg" srcset={challenge['image_url_jpeg']} />
-		<img src={challenge['image_url_jpeg']} alt={challenge['description']} />
-	</picture>
-{/if}
-{#if words && words.length > 0}
-	I am dreaming of {getWord(0, correctWords)}, {getWord(1, correctWords)}, and {getWord(2, correctWords)}
-{/if}
-<input bind:value={word} placeholder="What am I dreaming of?" />
-<button on:click={submitWord}>Submit</button>
-{#if showSuggestion}
-	<p>Perhaps you are thinking of
-		<button on:click={acceptSuggestion}>{suggestion}</button>
-	</p>
-{/if}
 
-<h3>Wrong Words</h3>
-<ul>
-	{#each guessedWords as word}
-		<li>{word}</li>
-	{/each}
-</ul>
+<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+	<!-- We've used 3xl here, but feel free to try other max-widths based on your needs -->
+	<div class="mx-auto max-w-3xl">
+		<div class="py-3"></div>
+		<!-- Content goes here -->
+		{#if challenge}
+			<picture class="shadow-lg">
+				<source type="image/webp" srcset={challenge['image_url_webp']} />
+				<source type="image/jpeg" srcset={challenge['image_url_jpeg']} />
+				<img src={challenge['image_url_jpeg']} alt={challenge['description']} />
+			</picture>
+		{/if}
+		{#if words && words.length > 0}
+			<div class="dark:text-slate-300 text-3xl font-serif">
+				I am dreaming of {getWord(0, correctWords)}, {getWord(1, correctWords)}, and {getWord(
+					2,
+					correctWords
+				)}
+			</div>
+		{/if}
+		<hr class="my-3" />
+		{#if correctWords.length !== answer.length}
+			<div class="mt-2">
+				<input
+					bind:value={word}
+					placeholder="What am I dreaming of?"
+					class="px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-800 sm:text-sm sm:leading-6"
+				/>
+			</div>
+			{#if showSuggestion}
+				<div class="dark:text-white">
+					<p>
+						Perhaps you are thinking of
+						<button on:click={acceptSuggestion} class="text-blue-300">{suggestion}</button>
+					</p>
+				</div>
+			{/if}
+		{/if}
 
-{#if correctWords.length !== answer.length}
-	<button on:click={reveal}>Reveal</button>
-{/if}
+		{#if guessedWords.length > 0}
+			<div class="dark:text-slate-400 my-3">
+				I don't think I was dreaming of {getGuessedWords(guessedWords)}...
+			</div>
+		{/if}
 
-{#if correctWords.length === answer.length && currentLevel < levels.length - 1}
-	<button on:click={nextLevel}>Next Level</button>
-{/if}
+		{#if correctWords.length !== answer.length}
+			<button
+				on:click={reveal}
+				class="rounded bg-white/10 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-white/20 my-6"
+				>Reveal
+			</button>
+		{/if}
+
+		{#if correctWords.length === answer.length && currentLevel < levels.length - 1}
+			<button
+				on:click={nextLevel}
+				class="rounded bg-white/10 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-white/20 my-6"
+				>Next Level
+			</button>
+		{/if}
+	</div>
+</div>
